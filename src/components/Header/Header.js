74638ebf7905f9656fc02800cb2adbd298746c97
Hannah Elliott <hgelliott90@gmail.com>
2021-02-21 12:14:26 -0500

    I broke things and then had to fix them.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Header.css'
+import ScrollReveal from 'scrollreveal'
+
+const Header = () => {
+
+    React.useEffect(() => {
+        ScrollReveal().reveal('.header', { delay: 700});
+        ScrollReveal().reveal('.header span', {delay: 2600});
+    }, [])
+
+    return (
+        <div>
+            <header>
+                <big className='header load-hidden'> 
+                    <span>c</span>osmic <br/>
+                    ch<span>i</span>ll <br/>
+                    loun<span>g</span>e
+                </big>
+            </header>
+        </div>
+    )
+}
+
+export default Header
