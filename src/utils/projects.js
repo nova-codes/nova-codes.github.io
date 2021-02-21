4a6fb664bb8374e12f469514adb4cf4713ee2438
Hannah Elliott <hgelliott90@gmail.com>
2021-02-21 11:55:18 -0500

    Updating projects with description and features.

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -4,42 +4,54 @@ const projects = [
         title: 'Bops and Hops',
         href: 'https://github.com/nova-codes/Bops-and-Hops',
         img: './images/bops-hops.png',
-        alt: 'Bops and Hops Demo'
+        alt: 'Bops and Hops Demo',
+        desc: 'A web application to randomly generate a beverage based on user input of artist and spirit.',
+        features: 'Non-Alcoholic beverages available as well.'
     },
     {
         id: 2,
         title: 'Minion Mingle',
         href: 'https://github.com/nova-codes/minion-mingle',
         img: './images/minion.png',
-        alt: 'Minion Mingle Demo'
+        alt: 'Minion Mingle Demo',
+        desc: 'A new dating application where you can find your one true love. Complete your profile and let our un-patented algorithm do the rest!',
+        features: 'Create an account and login to be matched with a fellow minion lover!'
     },
     {
         id: 3,
         title: 'Dream Burger Simulator',
         href: 'https://github.com/nova-codes/dream-burger-simulator',
         img: './images/burger-app.png',
-        alt: 'Dream Burger Simulator Demo'
+        alt: 'Dream Burger Simulator Demo',
+        desc: 'An interactive simulation game where you decide your burger-destiny.',
+        features: 'Choose your warrior and devour them to make them your destiny.'
     },
     {
         id: 4,
         title: 'Weather Dashboard',
         href: 'https://github.com/nova-codes/weather-dashboard',
         img: './images/weather.png',
-        alt: 'Weather Dashboard Demo'
+        alt: 'Weather Dashboard Demo',
+        desc: 'The goal of this website/app was to create a fully-functional weather dashboard utilizing two different OpenWeather APIs.',
+        features: 'Previous five searches are saved. Expanding five day forecast with UV index.'
     },
     {
         id: 5,
         title: 'Password Generator',
         href: 'https://github.com/nova-codes/password-generator',
         img: './images/pass-gen.png',
-        alt: 'Password Generator Demo'
+        alt: 'Password Generator Demo',
+        desc: 'A prompt-based, javascript-powered password generator',
+        features: 'Customize length and character sets. Once all characters are randomly selected, they are then shuffled before output for a truly secure password.'
     },
     {
         id: 6,
         title: 'Employee Summary',
         href: 'https://github.com/nova-codes/employee-summary',
         img: './images/emp-summary.png',
-        alt: 'Employee Summary Template Engine Demo'
+        alt: 'Employee Summary Template Engine Demo',
+        desc: 'A Node-CLI powered template engine for an employee summary page.',
+        features: 'User can add additional team members, and the application will output an HTML file that displays an organized summary of the information provided.'
     }
 ]
 
