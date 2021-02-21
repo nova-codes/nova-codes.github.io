957c88213741d3d8e43d881de8aca898477fff99
Hannah Elliott <hgelliott90@gmail.com>
2021-02-20 20:29:14 -0500

    Styled header component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './index.css';
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Card from './components/Card/Card'
+import Nav from './components/Nav/Nav'
+import Portfolio from './components/Portfolio/Portfolio'
 
 function App() {
   return (
     <div className="App">
-      i am a developer
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <big> <span>c</span>osmic <br/>
+                    ch<span>i</span>ll <br/>
+                    loun<span>g</span>e
+                </big>
+            </header>
+        </div>
+    )
+}
+
+export default Header
